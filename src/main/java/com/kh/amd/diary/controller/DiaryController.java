70a643ed07bc6eaaba86be609af477623c20784c
mo <mo@mo>
2019-06-21 15:39:29 +0900

    뷰페이지 생성

--- src/main/java/com/kh/amd/diary/controller/DiaryController.java
+++ src/main/java/com/kh/amd/diary/controller/DiaryController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DiaryController {
 	
-	@RequestMapping("diaryDetail.dd")
+	@RequestMapping("diaryDetail.di")
 	public String showDiaryDetail() {
 		
 		return "diary/diaryDetail";
