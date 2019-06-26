f1a7a57a53e2538426544d099639fc5157ddaa34
mo <mo@mo>
2019-06-26 21:13:42 +0900

    모모링 머지

--- src/main/java/com/kh/amd/survey/model/service/SurveyService.java
+++ src/main/java/com/kh/amd/survey/model/service/SurveyService.java
@@ -1,16 +1,23 @@
 package com.kh.amd.survey.model.service;
 
+
+import java.util.List;
+
 import com.kh.amd.survey.model.vo.Survey;
 
 public interface SurveyService {
 
-	int insertSurvey1(Survey s);
+	int insertSurvey1(Survey s, String mno);
 
 	int insertSurvey2(Survey s);
 
 	int insertSurvey3(Survey s);
 
-	int updateSurvey(Survey s); 
+	int updateSurvey(Survey s);
+
+	List<Survey> surveyList(int mno);
+
+	
 
 	
 
