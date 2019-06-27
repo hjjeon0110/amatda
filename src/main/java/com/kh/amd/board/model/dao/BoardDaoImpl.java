47cead343ecf84b1a685121d2ff126aa3a905488
KimSunAh <KimSunAh@DESKTOP-HERS0S3>
2019-06-27 21:07:07 +0900

    공지사항 리스트, 상세보기, 조회수증가

--- src/main/java/com/kh/amd/board/model/dao/BoardDaoImpl.java
+++ src/main/java/com/kh/amd/board/model/dao/BoardDaoImpl.java
@@ -1,5 +1,7 @@
 package com.kh.amd.board.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -36,6 +38,24 @@ public class BoardDaoImpl implements BoardDao {
 	public int insertQna(SqlSessionTemplate sqlSession, Board b) {
 		return sqlSession.insert("Board.insertQna",b);
 	
+	}
+
+	//공지사항 리스트(김선아)
+	@Override
+	public List<Board> noticeList(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectList("Board.noticeList");
+	}
+	
+	//공지사항 상세보기(김선아)
+	@Override
+	public Object noticeSelectOne(SqlSessionTemplate sqlSession, int bNo) {
+		return sqlSession.selectOne("Board.noticeSelectOne", bNo);
+	}
+
+	//공지사항 조회수 증가(김선아)
+	@Override
+	public void increaseViewcnt(SqlSessionTemplate sqlSession, int bNo) {
+		sqlSession.update("Board.increaseViewcnt", bNo);
 	}
 
 	/*
--- src/main/java/com/kh/amd/board/model/service/BoardService.java
+++ src/main/java/com/kh/amd/board/model/service/BoardService.java
@@ -1,5 +1,9 @@
 package com.kh.amd.board.model.service;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import com.kh.amd.board.model.vo.Board;
 
 public interface BoardService {
@@ -21,6 +25,16 @@ public interface BoardService {
 	 */
 
 	int insertQna(Board b);
+	
+	//공지사항 리스트(김선아)
+	List<Board> noticeList();
+
+	//공지사항 상세보기(김선아)
+	Object noticeSelectOne(int bNo);
+
+	//공지사항 조회수 증가(김선아)
+	void increaseViewcnt(int bNo, HttpSession session);
+
 
 	/* int selectReview(Board b); */
 
