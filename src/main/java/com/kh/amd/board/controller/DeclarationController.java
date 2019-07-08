a2d3f3a392edecbecec756819f3ac79ffbb42013
y_s_r <y_s_r@DESKTOP-3TA4RSV>
2019-07-08 21:11:11 +0900

    7월8일 육소라 머지( 신고게시판 사진업로드 설정도중)

--- src/main/java/com/kh/amd/board/controller/DeclarationController.java
+++ src/main/java/com/kh/amd/board/controller/DeclarationController.java
@@ -1,12 +1,21 @@
 package com.kh.amd.board.controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.amd.board.model.service.DeclarationService;
 import com.kh.amd.board.model.vo.Declaration;
+import com.kh.amd.common.CommonUtils;
+import com.kh.amd.member.model.vo.Member;
 
 @Controller
 public class DeclarationController {
@@ -26,11 +35,41 @@ public class DeclarationController {
 	
 	//신고게시판 입력(sr)
 		@RequestMapping("insertDeclaration.de")
-		public String insertDeclaration(Model model,Declaration d) {
-			System.out.println("신고게시판인 내가 호출됐어요!!!");
-			System.out.println(d);
+		public String insertDeclaration(Model model,Declaration d,Member m,HttpServletRequest request,
+										@RequestParam(name="declImgFile", required=false) MultipartFile declImgFile) {
+			String mno = request.getParameter("mNo");
+			ds.insertDeclaration(d,mno);
 			
-			int result = ds.insertDeclaration(d);
-			return "board/insertDeclaration";
+			int bno = ds.selectDeclBno();
+			System.out.println("bno in declController : " + bno);
+			
+			System.out.println("신고게시판인 내가 호출됐어요!!!");
+			System.out.println("d in declController : " + d);
+			System.out.println("mno in declController : " + mno);
+			
+			String root = request.getSession().getServletContext().getRealPath("resources");
+			
+			String filePath = root + "\\uploadFiles";		
+			String originalFilename = declImgFile.getOriginalFilename();
+			String ext = originalFilename.substring(originalFilename.lastIndexOf(".")); 
+			String changeName = CommonUtils.getRandomString();
+			
+
+				try {
+					
+					declImgFile.transferTo(new File(filePath + "\\" + changeName + ext));
+							
+					ds.insertDeclarationImg(bno, mno, filePath, originalFilename, changeName, ext);
+					
+					return "redirect:insertDeclaration.de";
+					
+				}catch (IllegalStateException | IOException e) {
+					e.printStackTrace();
+					System.out.println("에러발생");
+				}
+
+				return "board/insertDeclaration";		
+			
+			}
+			
 		}
-}
