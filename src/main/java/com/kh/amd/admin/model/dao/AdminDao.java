34120dd42ddaf41188cd1765062fa2f50493dca8
KimSunAh <KimSunAh@DESKTOP-HERS0S3>
2019-07-10 13:27:56 +0900

    휴

--- src/main/java/com/kh/amd/admin/model/dao/AdminDao.java
+++ src/main/java/com/kh/amd/admin/model/dao/AdminDao.java
@@ -37,6 +37,9 @@ public interface AdminDao {
 	//환불 승인 처리
 	int refundAgree(SqlSessionTemplate sqlSession, int payNo);
 
+	//환불 거절 처리
+	int refundRefuse(SqlSessionTemplate sqlSession, int payNo);
+
 
 
 
