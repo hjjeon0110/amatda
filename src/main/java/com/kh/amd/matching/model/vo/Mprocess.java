3ea4737ef88fdff81a17f2ac64d64ba2fc6db713
kimjinhwan <kimjinhwan@DESKTOP-MDBACOL>
2019-07-13 21:10:01 +0900

    190713? 했어요

--- src/main/java/com/kh/amd/matching/model/vo/Mprocess.java
+++ src/main/java/com/kh/amd/matching/model/vo/Mprocess.java
@@ -15,13 +15,20 @@ public class Mprocess implements java.io.Serializable {
 	private String matchingStatus;
 	private Date processDate;
 	
+	//DB에서받아와서 견적서 내용을 보여주기 위한 필드 선언(김진환)
+	private String matchEstName;
+	private int matchEstDay;
+	private String matchEstContents;
+	private String matchEstPrice;
+	
 	//매칭견적서 컬랙션 추가(김진환)
 	private Estimate matchEstimate;
 	
 	public Mprocess() {}
 
 	public Mprocess(int processNo, int uno, int tno, int matchingLevel, String transferStatus, String matchingAccept,
-			String matchingStatus, Date processDate, Estimate matchEstimate) {
+			String matchingStatus, Date processDate, String matchEstName, int matchEstDay, String matchEstContents,
+			String matchEstPrice, Estimate matchEstimate) {
 		super();
 		this.processNo = processNo;
 		this.uno = uno;
@@ -31,6 +38,10 @@ public class Mprocess implements java.io.Serializable {
 		this.matchingAccept = matchingAccept;
 		this.matchingStatus = matchingStatus;
 		this.processDate = processDate;
+		this.matchEstName = matchEstName;
+		this.matchEstDay = matchEstDay;
+		this.matchEstContents = matchEstContents;
+		this.matchEstPrice = matchEstPrice;
 		this.matchEstimate = matchEstimate;
 	}
 
@@ -38,68 +49,100 @@ public class Mprocess implements java.io.Serializable {
 		return processNo;
 	}
 
-	public int getUno() {
-		return uno;
-	}
-
-	public int getTno() {
-		return tno;
-	}
-
-	public int getMatchingLevel() {
-		return matchingLevel;
-	}
-
-	public String getTransferStatus() {
-		return transferStatus;
-	}
-
-	public String getMatchingAccept() {
-		return matchingAccept;
-	}
-
-	public String getMatchingStatus() {
-		return matchingStatus;
-	}
-
-	public Date getProcessDate() {
-		return processDate;
-	}
-
-	public Estimate getMatchEstimate() {
-		return matchEstimate;
-	}
-
 	public void setProcessNo(int processNo) {
 		this.processNo = processNo;
+	}
+
+	public int getUno() {
+		return uno;
 	}
 
 	public void setUno(int uno) {
 		this.uno = uno;
 	}
 
+	public int getTno() {
+		return tno;
+	}
+
 	public void setTno(int tno) {
 		this.tno = tno;
+	}
+
+	public int getMatchingLevel() {
+		return matchingLevel;
 	}
 
 	public void setMatchingLevel(int matchingLevel) {
 		this.matchingLevel = matchingLevel;
 	}
 
+	public String getTransferStatus() {
+		return transferStatus;
+	}
+
 	public void setTransferStatus(String transferStatus) {
 		this.transferStatus = transferStatus;
+	}
+
+	public String getMatchingAccept() {
+		return matchingAccept;
 	}
 
 	public void setMatchingAccept(String matchingAccept) {
 		this.matchingAccept = matchingAccept;
 	}
 
+	public String getMatchingStatus() {
+		return matchingStatus;
+	}
+
 	public void setMatchingStatus(String matchingStatus) {
 		this.matchingStatus = matchingStatus;
 	}
 
+	public Date getProcessDate() {
+		return processDate;
+	}
+
 	public void setProcessDate(Date processDate) {
 		this.processDate = processDate;
+	}
+
+	public String getMatchEstName() {
+		return matchEstName;
+	}
+
+	public void setMatchEstName(String matchEstName) {
+		this.matchEstName = matchEstName;
+	}
+
+	public int getMatchEstDay() {
+		return matchEstDay;
+	}
+
+	public void setMatchEstDay(int matchEstDay) {
+		this.matchEstDay = matchEstDay;
+	}
+
+	public String getMatchEstContents() {
+		return matchEstContents;
+	}
+
+	public void setMatchEstContents(String matchEstContents) {
+		this.matchEstContents = matchEstContents;
+	}
+
+	public String getMatchEstPrice() {
+		return matchEstPrice;
+	}
+
+	public void setMatchEstPrice(String matchEstPrice) {
+		this.matchEstPrice = matchEstPrice;
+	}
+
+	public Estimate getMatchEstimate() {
+		return matchEstimate;
 	}
 
 	public void setMatchEstimate(Estimate matchEstimate) {
@@ -110,9 +153,13 @@ public class Mprocess implements java.io.Serializable {
 	public String toString() {
 		return "Mprocess [processNo=" + processNo + ", uno=" + uno + ", tno=" + tno + ", matchingLevel=" + matchingLevel
 				+ ", transferStatus=" + transferStatus + ", matchingAccept=" + matchingAccept + ", matchingStatus="
-				+ matchingStatus + ", processDate=" + processDate + ", matchEstimate=" + matchEstimate + "]";
+				+ matchingStatus + ", processDate=" + processDate + ", matchEstName=" + matchEstName + ", matchEstDay="
+				+ matchEstDay + ", matchEstContents=" + matchEstContents + ", matchEstPrice=" + matchEstPrice
+				+ ", matchEstimate=" + matchEstimate + "]";
 	}
+	
 
+	
 	
 	
 	
